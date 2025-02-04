7d2e301110c93248dc7f8a8f1a31d8fa8540a060
mpolynsky <39326292-mpolynsky@users.noreply.replit.com>
2025-02-04 09:48:57 +0000

    Agent query: Пожалуйста, попробуйте войти снова, используя: Логин: admin Пароль: admin Работает ли вход в систему теперь?
    Add user authentication and update dependencies.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/74f28105-0b3e-4dca-973e-8a69c1d6d330/ab591024-a8f2-42e0-a99e-016e9e71f76c.jpg

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash
 import os
 from sqlalchemy.orm import DeclarativeBase
@@ -19,15 +19,23 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 }
 
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from models import User, PageMetadata, Feedback
 import admin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
             login_user(user)
